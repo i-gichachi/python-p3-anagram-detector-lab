c71bf8519833deb3dde5756775b5172f711c8db4
Ian Mwaura Gichachi <iangichachi@gmail.com>
2023-09-19 11:27:16 +0300

    Complete Assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,5 @@
-# your code goes here!
+class Anagram:
+  def __init__(self,word):
+    self.word=word
+  def match(self,words):
+    return [word for word in words if sorted(self.word)==sorted(word)]
